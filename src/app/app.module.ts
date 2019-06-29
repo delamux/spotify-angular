b06b8cda482b87320c2f2a2d5f2916b532879d19
delAmux <luisdelamo@gmail.com>
2019-06-29 02:25:24 +0100

    ref:#master change styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { ArtistComponent } from './components/artist/artist.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    ArtistComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
